1b28dbac0c78e4cd0c6347d75de54f493390dc37
Mohammed Ali <fh0420@wayne.edu>
2017-07-29 14:49:06 -0400

    modied files

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
+var app = angular.module("APP", ["ngRoute"]);
+
+app.config(function($routeProvider, $locationProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "index.htm"
+        templateUrl : "index.html",
+        controller:"myController"
     })
     .when("/view1", {
-        templateUrl : "view1.htm"
+        templateUrl : "view1.html",
+        controller:"myController"
     })
     .when("/view2", {
-        templateUrl : "view2.htm"
+        templateUrl : "view2.html",
+        controller:"myController"
+
     })
-    .otherwise(, {
-        
+    .when("/view3", {
+        templateUrl : "view3.html",
+        controller:"myController"
+    })
+    .when("/view4", {
+        templateUrl : "view4.html",
+        controller:"myController"
+    })
+    .otherwise({
+        redirectTo:'/'
     });
+    $locationProvider.hashPrefix("");
 });
+app.controller("myController", function(scope){
+	$scope.files = {
+		view1: cat.png,
+		view2: dog.png,
+		view3: walrus.png, 
+		view4: ferret.png
+	};
+});
+
+
+
